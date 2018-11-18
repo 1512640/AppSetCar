caa91fdc5f20638caf014efdebc5afe13a674b1b
1512640 <1512640@student.hcmus.edu.vn>
2018-11-18 12:45:16 +0700

    Push app3
    List status
    Direction

--- back-end/app.js
+++ back-end/app.js
@@ -2,17 +2,20 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     cors = require('cors');
-    
-var app1Ctrl = require('./app1');
 
+var app1Ctrl = require('./app1');
+var app2Ctrl = require('./app2');
+var app3Ctrl = require('./app3');
 var app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+	type:'application/json' }));
 app.use(cors());
 
 app.use('/sendInform', app1Ctrl);
-
+app.use('/resInform', app2Ctrl);
+app.use('/getList', app3Ctrl);
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`API running on PORT ${PORT}`);
